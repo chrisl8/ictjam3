d13482194c2d54cca579da3d47050fbe28f2eb99
Charles Burnham <felixtheslayer@gmail.com>
2017-02-03 19:07:59 -0600

    change port to 8080 to work with cloud9

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express();
 
 app.use('/', express.static(__dirname + '/src', {index: 'index.html'}));
 
-app.listen(3000, function () {
-    console.log('Listening on port 3000');
+app.listen(8080, function () {
+    console.log('Listening on port 8080');
 });
