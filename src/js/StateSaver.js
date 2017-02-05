324de2bdb2e1b1f7692d6830e4bacb76ee8602ac
chrisl8 <chrisl8@users.noreply.github.com>
2017-02-05 11:51:01 -0600

    NPC5 fixed.

--- src/js/StateSaver.js
+++ src/js/StateSaver.js
@@ -6,7 +6,7 @@ var ICTJAM3 = scope.ICTJAM3;
 ICTJAM3.StateSaver = function () {
     this.currentMap = null;
     this.currentRandomNpcPosition = 0;
-    this.randomNpcPositions = [{ x: 117, y: 175  }, { x: 208, y: 141  }, { x: 195, y: 357  }, { x: 355, y: 276  }, { x: 599, y: 201  }];
+    this.randomNpcPositions = [{ x: 117, y: 175  }, { x: 208, y: 141  }, { x: 195, y: 357  }, { x: 355, y: 276  }, { x: 46, y: 254  }];
 };
 
 ICTJAM3.StateSaver.prototype = {
